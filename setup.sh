cb6cfb5fdee71d8f0dfed7a794bec7d6635647fc
ChirayuShishodiya <chirayu11@hotmail.co.uk>
2019-09-18 15:11:51 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+# Install Homebrew 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Use Brew cask to install basic apps
+brew cask install google-chrome
+brew cask install pycharm-ce
+brew cask install visual-studio-code
+brew cask install iterm2
+# Github Desktop
+brew cask install github
+brew cask install postman
+brew cask install cyberduck
+brew cask install sequel-pro
+brew cask install slack
+brew cask install docker
+brew cask install libreoffice
