93c03aae9dbdc4ef29d3064252cddc623761aabe
Tom <tom.coathup@growthintelligence.com>
2020-05-14 22:21:36 +0100

    setup.sh: Added Java, MySQL, GPG, AWS

--- setup.sh
+++ setup.sh
@@ -33,4 +33,36 @@ echo "export WORKON_HOME=$HOME/.virtualenvs" >> $HOME/.zshrc
 echo "source /usr/local/bin/virtualenvwrapper.sh" >> $HOME/.zshrc
 brew install pyenv-virtualenvwrapper
 echo "export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV=true" >> $HOME/.zshrc
+
+# Java 8
+brew tap adoptopenjdk/openjdk
+brew cask install adoptopenjdk8
+
+# MySQL Client
+brew install mysql@5.7
+brew cask install mysql-connector-python
+brew install mysql-connector-c
+echo "PATH=$PATH:/usr/local/Cellar/mysql@5.7/5.7.29/bin" >> ~/.zshrc
+echo "DYLD_LIOBRARY_PATH=/usr/local/Cellar/mysql@5.7/5.7.29/lib:$DYLD_LIBRARY_PATH" >> ~/.zshrc
+
+# GPG
+brew install gnupg
+
+# AWS
+mkdir ~/.aws/
+touch ~/.aws/config
+touch ~/.aws/credentials
+echo "[default]
+region = eu-west-1" >> ~/.aws/config
+echo "[default]
+aws_access_key_id = YOUR_KEY
+aws_secret_access_key = YOUR_SECRET" >> ~/.aws/credentials
+
+# Logging Directory
+sudo chown $USER /var/log/
+
+# Local Working Directory
+mkdir $HOME/growthintel
+
+echo "GI_ENV=DEV" >> ~/.zshrc
 exec "$SHELL"
